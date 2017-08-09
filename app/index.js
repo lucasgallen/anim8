f6a6e6ffb146a4e1b42c45262b6c2d870ba27cc3
Lucas Allen <lucas.g.allen@gmail.com>
2017-08-09 13:17:05 +0300

    Add simple drawing actions to canvas component.

--- app/index.js
+++ app/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class DrawPassCanvas extends React.Component {
+    componentDidMount() {
+        var canvasContext = document.getElementById('drawpass-canvas').getContext('2d');
+
+        this.canvasContext = canvasContext;
+        this.isPenDown = false;
+    }
+
+    startPath() {
+        this.canvasContext.beginPath();
+        this.isPenDown = true;
+    }
+
+    drawPath(e) {
+        if (this.isPenDown) {
+            this.canvasContext.lineTo(e.clientX, e.clientY);
+            this.canvasContext.stroke();
+        }
+    }
+
+    endPath() {
+        this.isPenDown = false;
+    }
+
     render() {
         return (
-            <canvas id="drawpass-canvas"></canvas>
+            <canvas
+                onMouseDown={() => this.startPath()}
+                onMouseMove={(e) => this.drawPath(e)}
+                onMouseUp={() => this.endPath()}
+                id="drawpass-canvas"
+            ></canvas>
         );
     }
 }
