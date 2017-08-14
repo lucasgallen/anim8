cb76226b2b9c11a9f7aaa11b780dc0417d19f27e
Lucas Allen <lucas.g.allen@gmail.com>
2017-08-14 16:43:35 +0300

    Show the previous page in Flipbook component.

--- app/components/Flipbook.jsx
+++ app/components/Flipbook.jsx
@@ -12,6 +12,7 @@ class Flipbook extends React.Component {
 
     componentDidMount() {
         this.canvasImg = '';
+        this.shadowImg = '';
     }
 
     prevPage() {
@@ -55,22 +56,46 @@ class Flipbook extends React.Component {
 
     clearPage() {
         let canvas = this.canvasComponent.canvas;
+        let shadowCanvas = this.shadowCanvas.canvas;
         let ctx = canvas.getContext('2d');
+        let shadowCtx = shadowCanvas.getContext('2d');
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        shadowCtx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     render() {
         let canvasImg = '';
+        let shadowImg = '';
 
         if (this.props.pages.length && this.state.page - 1 < this.props.pages.length) {
             canvasImg = this.props.pages[this.state.page - 1].canvasImg;
         }
 
+        if (this.props.pages.length && this.state.page - 2 >= 0) {
+            shadowImg = this.props.pages[this.state.page - 2].canvasImg;
+        } else {
+            shadowImg = '';
+        }
+
         return (
             <div>
                 <h2>Page: {this.state.page}</h2>
-                <Canvas store={this.props.store} canvasImg={canvasImg} ref={(canvas) => this.canvasComponent = canvas} />
+                <Canvas
+                    store={this.props.store}
+                    renderUI={true}
+                    canvasImg={canvasImg}
+                    ref={(canvas) => this.canvasComponent = canvas}
+                    style={{ position: 'relative', zIndex: 2 }}
+                />
+
+                <Canvas
+                    store={this.props.store}
+                    isShadow={true}
+                    canvasImg={shadowImg}
+                    ref={(canvas) => this.shadowCanvas = canvas}
+                    style={{ position: 'absolute', top: '68px' }}
+                />
 
                 <button
                     onClick={() => this.prevPage()}
