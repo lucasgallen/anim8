96362bb350824bd17ff432f3d468ee47b88e4f0b
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-11 09:36:54 +0000

    Configure webpack to serve ENV vars and proxy api calls

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const dotenv = require('dotenv');
 const { merge } = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,53 +10,67 @@ const PATHS = {
   app: path.join(__dirname, 'app'),
   node_modules: path.join(__dirname, 'node_modules'),
   build: path.join(__dirname, '/'),
+  env: path.join(__dirname, '/.env'),
 };
 
-const commonConfig = merge([
-  {
-    resolve: {
-      modules: ['node_modules'],
-      extensions: ['.js', '.jsx'],
-      alias: {
-        gif$: `${__dirname}/app/gif/gif.js`,
-      },
-    },
-    entry: {
-      app: PATHS.app,
-    },
-    output: {
-      publicPath: '/',
-      filename: '[name].js',
-    },
-    plugins: [
-      new CleanObsoleteChunks(),
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: path.join(__dirname, 'public/index.html'),
-      }),
-    ],
-  },
-  {
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: PATHS.app,
-          exclude: [
-            /gif/,
-            /node_modules/,
-          ],
+const getEnvKeys = env => {
+  const fileEnv = dotenv.config({ path: `${PATHS.env}.${env['ENVIRONMENT']}` }).parsed;
 
-          use: ['babel-loader', 'eslint-loader'],
+  return Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+
+    return prev;
+  }, {});
+};
+
+const commonConfig = envKeys => {
+  return merge([
+    {
+      resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx'],
+        alias: {
+          gif$: `${__dirname}/app/gif/gif.js`,
         },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
+      },
+      entry: {
+        app: PATHS.app,
+      },
+      output: {
+        publicPath: '/',
+        filename: '[name].js',
+      },
+      plugins: [
+        new CleanObsoleteChunks(),
+        new webpack.DefinePlugin(envKeys),
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: path.join(__dirname, 'public/index.html'),
+        }),
       ],
     },
-  }
-]);
+    {
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            include: PATHS.app,
+            exclude: [
+              /gif/,
+              /node_modules/,
+            ],
+
+            use: ['babel-loader', 'eslint-loader'],
+          },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+    }
+  ]);
+};
 
 const productionConfig = merge([
   {
@@ -79,33 +94,40 @@ const productionConfig = merge([
   },
 ]);
 
-const developmentConfig = merge([
-  {
-    mode: 'development',
-    devServer: {
-      clientLogLevel: 'debug',
-      historyApiFallback: true,
-      stats: {
-        errors: true,
-      },
+const developmentConfig = envKeys => {
+  return merge([
+    {
+      mode: 'development',
+      devServer: {
+        clientLogLevel: 'debug',
+        historyApiFallback: true,
+        stats: {
+          errors: true,
+        },
 
-      host: '0.0.0.0',
-      port: '8080',
+        host: '0.0.0.0',
+        port: '8080',
+        proxy: {
+          '/api': `http://localhost:${process.env.API_DEV_SERVER_PORT}`
+        },
 
-      publicPath: '/',
+        publicPath: '/',
 
-      overlay: {
-        errors: true,
-        warnings: true,
+        overlay: {
+          errors: true,
+          warnings: true,
+        },
       },
     },
-  },
-]);
+  ]);
+};
 
 module.exports = (env) => {
+  const envKeys = getEnvKeys(env);
+
   if (env === 'production') {
-    return merge(commonConfig, productionConfig);
+    return merge(commonConfig(envKeys), productionConfig);
   }
 
-  return merge(commonConfig, developmentConfig);
+  return merge(commonConfig(envKeys), developmentConfig(envKeys));
 };
