97a28922198394e59179cbf929bccfd9427f399e
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-12 13:00:17 +0000

    Let loading component standout more

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  width: 100vw;
+`;
+
+const LoadingBlock = styled.div`
+  align-items: center;
+  bottom: 0;
+  box-shadow: 0.5rem 0 10px 0 #00000085;
+  display: flex;
+  font-size: calc(2.5rem + 1vw);
+  justify-content: center;
+  position: absolute;
+  left: 0;
+  width: 100vw;
+  top: 0;
+`;
+
+const Copy = styled.p`
+`;
 
 function Loading() {
   return (
-    <p>LOADING...</p>
+    <Container>
+      <LoadingBlock>
+        <Copy>LOADING...</Copy>
+      </LoadingBlock>
+    </Container>
   );
 }
 
