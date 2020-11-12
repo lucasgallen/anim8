7a160b536ec44155a529d05dfa0daaf360feb26f
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-12 16:24:10 +0000

    Make loading component a little more presentable

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  height: 100vh;
+  bottom: 0;
+  left: 0;
   overflow: hidden;
-  position: relative;
-  width: 100vw;
+  position: absolute;
+  right: 0;
+  top: 0;
 `;
 
 const LoadingBlock = styled.div`
   align-items: center;
   bottom: 0;
-  box-shadow: 0.5rem 0 10px 0 #00000085;
   display: flex;
   font-size: calc(2.5rem + 1vw);
   justify-content: center;
   position: absolute;
   left: 0;
-  width: 100vw;
+  right: 0;
   top: 0;
 `;
 
