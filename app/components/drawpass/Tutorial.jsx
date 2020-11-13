241e0fbdc7515a2a69943f32f3f0a1b38d4e4e35
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-13 17:08:15 +0000

    Convert Tutorial to function component

--- app/components/drawpass/Tutorial.jsx
+++ app/components/drawpass/Tutorial.jsx
@@ -1,52 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { changeStage } from '../../actions/drawpass.js';
 import TutorialSlide from './TutorialSlide';
 
-class Tutorial extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { step: 0 };
+const Tutorial = props => {
+  const [step, setStep] = useState(0);
 
-    this.slides = [
-      {
-        copy: 'DrawPass allows you to start a drawing and pass it along to a friend to collaborate on with a simple URL.',
-        imagePath: ''
-      },{
-        copy: 'First draw on the canvas with your mouse and the provided UI.',
-        imagePath: ''
-      },{
-        copy: 'Then when you\'re ready for a friend to add to the drawing, simply click "save" and share the provided URL!',
-        imagePath: ''
-      },
-    ];
-  }
+  const slides = [
+    {
+      copy: 'DrawPass allows you to start a drawing and pass it along to a friend to collaborate on with a simple URL.',
+      imagePath: ''
+    },{
+      copy: 'First draw on the canvas with your mouse and the provided UI.',
+      imagePath: ''
+    },{
+      copy: 'Then when you\'re ready for a friend to add to the drawing, simply click "save" and share the provided URL!',
+      imagePath: ''
+    },
+  ];
 
-  prevStage() {
-    const currentStep = this.state.step;
-    this.setState({ step: currentStep - 1 });
-  }
+  const prevStage = () => {
+    const currentStep = step;
+    setStep(currentStep - 1);
+  };
 
-  nextStage() {
-    const currentStep = this.state.step;
-    this.setState({ step: currentStep + 1 });
-  }
+  const nextStage = () => {
+    const currentStep = step;
+    setStep(currentStep + 1);
+  };
 
-  render() {
-    return (
-      <TutorialSlide
-        slide={ this.slides[this.state.step] }
-        prevStage={() => this.prevStage()}
-        nextStage={() => this.nextStage()}
-        createNewSession={() => this.props.createNewSession()}
-        first={ this.state.step === 0 }
-        last={ this.state.step === this.slides.length - 1 }
-      />
-    );
-  }
-}
+  return (
+    <TutorialSlide
+      slide={slides[step]}
+      prevStage={() => prevStage()}
+      nextStage={() => nextStage()}
+      createNewSession={props.createNewSession()}
+      first={step === 0}
+      last={step === slides.length - 1 }
+    />
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({ changeStage }, dispatch);
