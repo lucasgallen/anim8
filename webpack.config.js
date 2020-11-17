22dc981d27c86ff606f320c27f7d2979611b3211
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-17 15:07:27 +0000

    Let prod config receive env vars as well

--- webpack.config.js
+++ webpack.config.js
@@ -23,86 +23,92 @@ const getEnvKeys = env => {
   }, {});
 };
 
-const commonConfig = envKeys => {
+const commonConfig = merge([
+  {
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.js', '.jsx'],
+      alias: {
+        gif$: `${__dirname}/app/gif/gif.js`,
+      },
+    },
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      publicPath: '/',
+      filename: '[name].js',
+    },
+    plugins: [
+      new CleanObsoleteChunks(),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.join(__dirname, 'public/index.html'),
+      }),
+    ],
+  },
+  {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
+        },
+        {
+          test: /\.jsx?$/,
+          include: PATHS.app,
+          exclude: [
+            /gif/,
+            /node_modules/,
+          ],
+
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  }
+]);
+
+const productionConfig = envKeys => {
   return merge([
     {
-      resolve: {
-        modules: ['node_modules'],
-        extensions: ['.js', '.jsx'],
-        alias: {
-          gif$: `${__dirname}/app/gif/gif.js`,
-        },
-      },
-      entry: {
-        app: PATHS.app,
-      },
-      output: {
-        publicPath: '/',
-        filename: '[name].js',
-      },
-      plugins: [
-        new CleanObsoleteChunks(),
-        new webpack.DefinePlugin(envKeys),
-        new HtmlWebpackPlugin({
-          inject: false,
-          template: path.join(__dirname, 'public/index.html'),
-        }),
-      ],
-    },
-    {
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            enforce: 'pre',
-            use: ['source-map-loader'],
-          },
-          {
-            test: /\.jsx?$/,
-            include: PATHS.app,
-            exclude: [
-              /gif/,
-              /node_modules/,
-            ],
+      mode: 'production',
+      optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              mangle: true,
+              ie8: false,
+              keep_fnames: true,
+            },
+            extractComments: false,
+          }),
 
-            use: ['babel-loader', 'eslint-loader'],
-          },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
+          new webpack.DefinePlugin(merge(
+            envKeys,
+            { 'process.env.NODE_ENV': JSON.stringify('production') }
+          )),
         ],
       },
-    }
+    },
   ]);
 };
-
-const productionConfig = merge([
-  {
-    mode: 'production',
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            mangle: true,
-            ie8: false,
-            keep_fnames: true,
-          },
-          extractComments: false,
-        }),
-
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-      ],
-    },
-  },
-]);
 
 const developmentConfig = envKeys => {
   return merge([
     {
       mode: 'development',
+      plugins: [
+        new webpack.DefinePlugin(merge(
+          envKeys,
+          { 'process.env.NODE_ENV': JSON.stringify('development') }
+        )),
+      ],
       devServer: {
         clientLogLevel: 'debug',
         historyApiFallback: true,
@@ -131,8 +137,8 @@ module.exports = (env) => {
   const envKeys = getEnvKeys(env);
 
   if (env === 'production') {
-    return merge(commonConfig(envKeys), productionConfig);
+    return merge(commonConfig, productionConfig(envKeys));
   }
 
-  return merge(commonConfig(envKeys), developmentConfig(envKeys));
+  return merge(commonConfig, developmentConfig(envKeys));
 };
