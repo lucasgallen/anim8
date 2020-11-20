2a38e1d508d1b3facb02907305545ef562ac7016
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-20 16:05:37 +0000

    Clean up clear canvas button

--- app/components/canvas/ClearCanvasButton.jsx
+++ app/components/canvas/ClearCanvasButton.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import { savePage } from '../../actions/flipbook.js';
 import { ClearButton } from './styles';
 
-class ClearCanvasButton extends React.Component {
-  clearPage() {
-    const canvas = this.props.targetCanvas;
+function ClearCanvasButton(props) {
+  const clearPage = () => {
+    const canvas = props.targetCanvas;
     canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
-  }
+  };
 
-  render() {
-    return (
-      <ClearButton
-        onClick={() => this.clearPage()}
-        side='inherit'
-      >clear page</ClearButton>
-    );
-  }
+  return (
+    <ClearButton
+      onClick={() => clearPage()}
+      side='inherit'
+    >clear page</ClearButton>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    pages: state.pages,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ savePage }, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ClearCanvasButton);
+export default ClearCanvasButton;
