dcdd030262f2acd593e6c5cbd199d09da5c71457
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-27 13:55:15 +0000

    Remove build contents before recompiling production

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ yarn install --frozen-lockfile --check-files --silent
 
 printf "API_SERVER=$API_SERVER\nAPI_TOKEN=$API_TOKEN\n" > .env.production
 
+# Empty previous build contents
+rm  build/*
+
 yarn build
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
