5d386c1e7d3de6f0ae5e0b7d0da7c2183f81c757
Lucas Allen <lucas.g.allen@gmail.com>
2021-01-27 10:22:39 +0000

    Alphabetize redux store initial state

--- app/store/index.js
+++ app/store/index.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  pages: [],
   drawing: {},
+  pages: [],
   screen: {
     height: window.screen.availHeight,
     width: window.screen.availWidth,
